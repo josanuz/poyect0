355335f79fb64880f6890a832409425cdf9b69e9
asvaaron <asvaaron@gmail.com>
2014-04-11 13:37:24 -0600

    main mofificado

--- main.c
+++ main.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 #include "Puente.h"
 #include "carro.h"
 #include "params.h"
 #include "lista.h"
 #include "semaforos.h"
 #include "oficial.h"
-#include <time.h>
+
 short toExit = 1;
 Puente * puente; 
 Carro ** carros;
-List * l = init_list();
+List * l;
 pthread_t printer;
+int MAX;
+long longpuente;
+int distribucion,velauto,duracion,cantoficial,cantmaxcars;
+
+
+void leer(char * path){
+FILE *file;
+file=fopen(path,"r");
+if(file){
+	fscanf(file,"%ld %d %d %d %d %d",&longpuente,&distribucion,&velauto,&duracion,&cantoficial,&cantmaxcars);
+	printf("%ld %d %d %d %d %d",longpuente,distribucion,velauto,duracion,cantoficial,cantmaxcars);
+	sleep(5);
+	}
+}
+double distri(int m){
+	srand(time(NULL ));
+	double r = (double) rand() / RAND_MAX + 1;
+	return (-m) * log(r);
+}
 
 void * printCars(void * argc){
+	l = init_list();
 	int i = 0, x, dis;
 	dis = *((int *)argc);
 	while(toExit){
@@ -33,24 +55,14 @@ void * printCars(void * argc){
 		}
 		//usleep(1000);
 		printf("\033[7;0H   ");
-		printf("\033[7;0H %d",puente->cant_carros);
+		printf("\033[7;0H %ld",puente->cant_carros);
 	}
 	pthread_exit(NULL);
 }
 
-int main(int argc, char **argv){
+void FIFO(){
 	int i;
-	printf("Iniciar\n");
-	puente = puente_init(atoi(argv[1]));
-	int MAX = atoi(argv[2]);
-	
-	carros = (Carro**) malloc( MAX * sizeof(Carro*) );
-	for(i = 0; i< MAX ; i++) carros[i] = carro_init(1);
-	pthread_create(&printer,NULL,printCars,&puente->distancia);
-	
-	printf("Arrancara en 2..");
-	sleep(2);
-	/*for(i = 0; i< MAX ; i++){ // Normal Pasa Cuando Puede
+for(i = 0; i< MAX ; i++){ // Normal Pasa Cuando Puede
 		params * arg;
 		srand(time(0) + getpid() + rand()*(rand()%10));
 		arg = init_parametros(carros[i],puente,i % 2 ? IZQUIERDA : DERECHA);
@@ -58,8 +70,15 @@ int main(int argc, char **argv){
 		usleep(1 + rand()%1000);
 		pthread_create(carros[i]->thread,NULL,ingresar,(void*)arg);
 	}
-	for(i = 0; i< MAX ; i++) pthread_join(*carros[i]->thread, NULL);*/
-	arrancarOficial(puente, 2);
+	for(i = 0; i< MAX ; i++) 
+	pthread_join(*carros[i]->thread, NULL);
+	
+}
+
+void OficialTransito(){
+	
+	
+	arrancarOficial(puente, 2);//Poner en un metodo oficial
 	for(i = 0; i < MAX ; i++){
 		params * arg;
 		srand(time(0) + getpid() + rand()*(rand()%10));
@@ -73,3 +92,27 @@ int main(int argc, char **argv){
 	detenerOficial();
 	toExit = 0;
 }
+
+int main(int argc, char **argv){
+	int i;
+	leer(argv[1]);
+	printf("Iniciar\n");
+	puente = puente_init(longpuente);
+	MAX = cantmaxcars;
+	
+	carros = (Carro**) malloc( MAX * sizeof(Carro*) );
+	for(i = 0; i< MAX ; i++) carros[i] = carro_init(1);
+	pthread_create(&printer,NULL,printCars,&puente->distancia);
+	
+	
+	printf("Arrancara en 2..");
+	sleep(2);
+	
+	
+	
+	
+}
+
+
+
+
